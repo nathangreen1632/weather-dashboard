d113ed68673388e3c41a51d21ee400f8e1d207a0
Nathan Green <ngreen1632@gmail.com>
2024-12-12 14:09:49 -0600

    Updates null operator in server.ts. Removes TODOs

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,16 +7,13 @@ import routes from './routes/index.js';
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ?? 3001;
 
-// TODO: Serve static files of entire client dist folder
 app.use(express.static('client/dist'));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-
-// TODO: Implement middleware to connect the routes
 app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
 // Start the server on the port
